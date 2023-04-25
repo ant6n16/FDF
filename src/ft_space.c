ef75a88eedfcc956e0f29fe3ff13792b3ff152ba
Antonio Delgado Bejarano <ant6n16@MacBook-Pro-de-Antonio.local>
2023-04-25 15:01:56 +0200

    Dudas

--- src/ft_space.c
+++ src/ft_space.c
@@ -6,7 +6,7 @@
 /*   By: antdelga <antdelga@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 00:29:51 by antdelga          #+#    #+#             */
-/*   Updated: 2023/04/25 14:22:57 by antdelga         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:59:32 by antdelga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,35 +30,38 @@ void	ft_set_coords(int i, t_coords c, t_packet *pack)
 		pack->points[i].yiso = (0.866 * c.y + 0.5 * c.x - c.z);
 }
 
-void	ft_views(t_packet	*pack)
+void	ft_views(t_packet *pack)
 {
 	int			i;
 	t_coords	coord;
 
 	i = -1;
 
+	/* ¿POR QUÉ ESTOS VALORES? */
 	pack->zoom = 0;
 	pack->z_zoom = 0.4;
 	pack->x_zoom = 1;
 	pack->radians = 0;
 	pack->radians2 = 0;
 
+	/* ¿POR QUÉ CALCULA EL ZOOM ASÍ ? */
 	ft_checkzoom(pack);
 	while (++i < (pack->height * pack->width))
 	{
+
+		/* ¿DE DÓNDE SALEN ESTAS FÓRMULAS? */
 		coord.x = (pack->x_zoom * (i % pack->width) * pack->zoom) * \
 		cos(pack->radians) - ((i / pack->width) * pack->zoom) * \
 		sin(pack->radians);
-
 		coord.y = ((i / pack->width) * pack->zoom) * cos(pack->radians) \
 		* cos(pack->radians2) \
 		+ ((i % pack->width) * pack->zoom) * sin(pack->radians) \
 		- (pack->points[i].z * pack->zoom * pack->z_zoom) * sin(pack->radians2);
-
 		coord.z = (pack->points[i].z * pack->zoom * pack->z_zoom) * \
 		cos(pack->radians2) \
 		+ ((i / pack->width) * pack->zoom) * sin(pack->radians2);
-		
+
+		/* ¿DE DÓNDE SALEN LAS COORDENADAS ISOMÉTRICAS? */
 		ft_set_coords(i, coord, pack);
 	}
 }
